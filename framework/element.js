8179c68937072107d8ef8abbd5734649dda5ae80
James Anderson <james.anderson98@outlook.com>
2025-01-13 17:32:37 +0000

    Refactor createElement to improve readability

--- framework/element.js
+++ framework/element.js
@@ -1,9 +1,24 @@
-const createElement = tagName => (strings, ...args) => ({
-    type: tagName,
-    template: strings.reduce(
-        (acc, currentValue, index) => acc + currentValue + (args[index] || ""), "",
-    )
+import { h } from 'snabbdom';
+
+// initial state for reduce
+const initialState = {
+    template: ''
+};
+
+// custom reducer function to be used with reduce method
+const createReducer = args => (acc, currentString, index) => ({
+    ...acc,
+    template: acc.template + currentString + (args[index] || '')
 });
+
+const createElement = tagName => (strings, ...args) => {
+    const { template } = strings.reduce(createReducer(args), initialState);
+    
+    return {
+        type: 'element',
+        template: h(tagName, {}, template)
+    };
+};
 
 export const div = createElement('div');
 export const p = createElement('p');
