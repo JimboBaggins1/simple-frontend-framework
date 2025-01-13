65b6eb6666e00a7511ffc09eaac64c5b147d0604
James Anderson <james.anderson98@outlook.com>
2025-01-13 17:32:53 +0000

    Refactor init to use snabbdom
    Add onClick event handling
    
    Add event handling

--- framework/element.js
+++ framework/element.js
@@ -2,21 +2,29 @@ import { h } from 'snabbdom';
 
 // initial state for reduce
 const initialState = {
-    template: ''
+    template: '',
+    on: {}
 };
 
 // custom reducer function to be used with reduce method
-const createReducer = args => (acc, currentString, index) => ({
-    ...acc,
-    template: acc.template + currentString + (args[index] || '')
-});
+const createReducer = args => (acc, currentString, index) => {
+    const currentArg = args[index];
+
+    if (currentArg && currentArg.type === 'event')
+        return { ...acc, on: {click: currentArg.click } };
+
+    return {
+        ...acc,
+        template: acc.template + currentString + (args[index] || '')
+    };
+};
 
 const createElement = tagName => (strings, ...args) => {
-    const { template } = strings.reduce(createReducer(args), initialState);
+    const { template, on } = strings.reduce(createReducer(args), initialState);
     
     return {
         type: 'element',
-        template: h(tagName, {}, template)
+        template: h(tagName, { on }, template)
     };
 };
 
