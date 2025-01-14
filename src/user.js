2c6c86e9a5ef330b1221f60dd8d10878a61e9c43
James Anderson <james.anderson98@outlook.com>
2025-01-14 13:15:40 +0000

    Set initial state for User component. Add changeName method

--- src/user.js
+++ src/user.js
@@ -1,4 +1,19 @@
+import { createComponent } from '../framework';
 import { div } from '../framework/element';
+import { onClick } from '../framework/event';
 
-export const User = ({ firstName, lastName }) => 
-    div`Hello ${firstName} ${lastName}`;
+// initial state of User component
+const initialState = { firstName: 'James', lastName: 'Anderson' };
+
+// state mutating actions
+const methods = {
+    changeName: (state, firstName) => ({...state, firstName })
+};
+
+// component template
+const template = ({ firstName, lastName, methods }) =>
+    div`${onClick(() => methods.changeName('Amy'))} Hello ${firstName} ${lastName}`;
+
+
+// create real world component
+export const User = createComponent({ template, methods, initialState }); 
